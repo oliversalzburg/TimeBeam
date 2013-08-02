224df4a85dbff1e0bfcf42c1e1d7f9e747b46a9a
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-08-02 19:52:33 +0200

    Added initial Scrollbar class

---
+++ TimeBeam/Scrollbar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TimeBeam {
+  public partial class Scrollbar : UserControl {
+    private Bitmap PixelMap { get; set; }
+    private Graphics GraphicsContainer { get; set; }
+
+    public Scrollbar() {
+      InitializeComponent();
+      InitializePixelMap();
+    }
+
+    private void InitializePixelMap() {
+      PixelMap = new Bitmap( Width, Height );
+      GraphicsContainer = Graphics.FromImage( PixelMap );
+      GraphicsContainer.Clear( Color.Black );
+      Refresh();
+    }
+
+    private void ScrollbarResize( object sender, EventArgs e ) {
+      InitializePixelMap();
+    }
+
+    private void ScrollbarPaint( object sender, PaintEventArgs e ) {
+      e.Graphics.DrawImage( PixelMap, 0, 0 );
+    }
+  }
+}
